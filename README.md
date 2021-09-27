73ebb6060d9d682c04e8905b45f10316fbf81ec6
Renato <renatomercadoluna@hotmail.com>
2021-09-27 18:46:02 -0500

    compilation of contract complete ✅

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-### Voting problem
+# Voting problem
 
 This is a solution to the Voting problem given in the [solidity offical documentation](https://docs.soliditylang.org/en/v0.8.7/solidity-by-example.html)
 
 ⚒ work in progress
+
+0. Install stuff ( react app and hardhat and ethers related libs )
+
+1. Understand the contract
+
+2. Set up env
+
+3. Compile -> `npx hardhat compile`
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,7 @@
-require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -10,12 +9,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      // ⚠ Error realted to metamask usage 👇 look it in readme. Only necessary when using localhost testnet
+      //chainId: 1337,
+    },
+    // configuration for a test network
+    rinkeby: {
+      url: process.env.INFURA_NET,
+      accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
+    },
+  },
 };
